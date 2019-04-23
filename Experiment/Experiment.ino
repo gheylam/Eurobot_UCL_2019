115b2cad5212040e9d0d1a73fe62f4956ad1c146
Zheng Hung Chuah <zheng_hung@hotmail.com>
2019-04-23 19:30:24 +0100

    Experiment Added Contact switch to stop

--- Experiment/Experiment.ino
+++ Experiment/Experiment.ino
@@ -1,25 +1,32 @@
 #include <IRremote.h>
 
+#define CONTACT_END 4
+#define MOTOR_PIN 6
+
+
 const int RECV_PIN = 7;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 void setup(){
-  pinMode(6, OUTPUT);
-
+  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(CONTACT_END, INPUT_PULLUP);
   Serial.begin(9600);
   irrecv.enableIRIn();
   irrecv.blink13(true);
 
-  digitalWrite(6, LOW);
+  digitalWrite(MOTOR_PIN, LOW);
 
   
   while(!irrecv.decode(&results));
   Serial.println(results.value, HEX);
   irrecv.resume();
-  digitalWrite(6, HIGH);
+  digitalWrite(MOTOR_PIN, HIGH);
 }
 
 void loop(){
   // NOTHING
+  if(digitalRead(CONTACT_END)==0){
+    digitalWrite(MOTOR_PIN, LOW);
+  }
 }
